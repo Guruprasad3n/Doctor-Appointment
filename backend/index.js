a86da50a90e8c68df776ef190c8e19e66abd2470
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-03-05 14:29:26 +0530

    Doctor Backend

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require('cors');
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+
+// dotenv config
+dotenv.config()
+
+// rest object
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+// routes
+app.get("/", (req, res)=>{
+    res.status(200).send("You are in Home Page")
+})
+
+
+// listen Port
+const Port = process.env.PORT
+
+// listen
+app.listen(Port, (req, res)=>{
+    console.log(`Server Runnning in http://localhost:${Port}`)
+})
