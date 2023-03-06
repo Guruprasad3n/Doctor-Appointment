4e30f063f5571a8f6ffb53cbe257e8bc524bac73
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-03-06 13:26:46 +0530

    added

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -1,8 +1,18 @@
-function Register(){
-    return(
-        <div>
-            Register
-        </div>
-    )
+
+// import { useState } from "react";
+function Register() {
+//   const [name, setName] = useState();
+//   const [email, setEmail] = useState();
+//   const [password, setPassword] = useState();
+
+//   const handleRegister = () => {};
+
+  return (
+   <>
+<div className="bg-dark">
+    <h1 className="text-success">Hello</h1>
+</div>
+   </>
+  );
 }
-export default Register
+export default Register;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
+// import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ChakraProvider>
+      {/* <ChakraProvider> */}
         <App />
-      </ChakraProvider>
+      {/* </ChakraProvider> */}
     </BrowserRouter>
   </React.StrictMode>
 );
