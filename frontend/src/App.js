e45070fed69eb12222563cad20e06d664182cd30
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-03-06 12:32:09 +0530

    created Some pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
 
 function App() {
   return (
     < >
-     
+<Routes>
+  <Route path='/' element={<Home/>} />
+  <Route path='/login' element={<Login/>} />
+  <Route path='/register' element={<Register/>} />
+  </Routes>     
     </>
   );
 }
