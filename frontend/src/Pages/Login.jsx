a2b79de983150bc185fdb2d4f18184d62b229945
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-03-06 13:54:02 +0530

    login Register Frontend

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,8 +1,29 @@
+import "../Styles/Register.css"
+import { Form, Input } from "antd";
+import { Link, useNavigate } from "react-router-dom";
 function Login(){
+    const navigate = useNavigate()
+    const onFinishHandler = (values) => {
+      console.log(values);
+    };
+  
     return(
-        <div>
-            Login
-        </div>
+        <>
+             <div className="form-container">
+        <Form layout="vertical" onFinish={onFinishHandler} className="register-form"  >
+            <h3 className="text-center">Login</h3>
+         
+          <Form.Item label="Email" name={"email"}>
+            <Input type="email" required />
+          </Form.Item>
+          <Form.Item label="Password" name={"password"}>
+            <Input type="password" required />
+          </Form.Item>
+          <Link to={"/register"} className="m-2">Not A User Register Here</Link>
+          <button  className="btn btn-primary" type="submit">Login</button>
+        </Form>
+      </div>
+        </>
     )
 }
 export default Login
--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -1,18 +1,37 @@
-
 // import { useState } from "react";
+import "../Styles/Register.css"
+import { Form, Input } from "antd";
+import { Link, useNavigate } from "react-router-dom";
 function Register() {
-//   const [name, setName] = useState();
-//   const [email, setEmail] = useState();
-//   const [password, setPassword] = useState();
+  //   const [name, setName] = useState();
+  //   const [email, setEmail] = useState();
+  //   const [password, setPassword] = useState();
 
-//   const handleRegister = () => {};
+  //   const handleRegister = () => {};
+  const navigate = useNavigate()
+  const onFinishHandler = (values) => {
+    console.log(values);
+  };
 
   return (
-   <>
-<div className="bg-dark">
-    <h1 className="text-success">Hello</h1>
-</div>
-   </>
+    <>
+      <div className="form-container">
+        <Form layout="vertical" onFinish={onFinishHandler} className="register-form"  >
+            <h3 className="text-center">Register</h3>
+          <Form.Item label="Name" name={"name"}>
+            <Input type="text" required />
+          </Form.Item>
+          <Form.Item label="Email" name={"email"}>
+            <Input type="email" required />
+          </Form.Item>
+          <Form.Item label="Password" name={"password"}>
+            <Input type="password" required />
+          </Form.Item>
+          <Link to={"/login"} className="m-2">Already User Login Here</Link>
+          <button  className="btn btn-primary" type="submit">Register</button>
+        </Form>
+      </div>
+    </>
   );
 }
 export default Register;
