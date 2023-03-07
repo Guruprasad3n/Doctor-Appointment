645f16a8f7685a51dbd2c7f8b7c250fcea5e71a2
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-03-07 11:16:37 +0530

    userModel

--- backend/models/userMode.js
+++ backend/models/userMode.js
@@ -1,7 +1,19 @@
-const {Schema, model} = require("mongoose");
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
-    name:{
-        type:String
-    }
-})
+  name: {
+    type: String,
+    require: [true, "Name is Require"],
+  },
+  email: {
+    type: String,
+    require: [true, "Email is Require"],
+  },
+  password: {
+    type: String,
+    require: [true, "Password Is Require"],
+  },
+});
+
+const userModel = model("user", userSchema);
+module.exports = userModel;
