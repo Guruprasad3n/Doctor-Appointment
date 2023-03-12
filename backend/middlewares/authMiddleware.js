07495406065277c3f6f157ed6f91422eacdf4950
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-03-12 23:40:56 +0530

    Doc

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = async(req, res, next)=>{
-    
+    const token = req.headers['authorization'].split(" ")[1]
+    jwt.verify(token, process.env.JWT_SECRET)
 }
