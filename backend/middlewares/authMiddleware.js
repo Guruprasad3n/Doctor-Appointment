2ba1c379a7d03348f93b0d2b4bbe6b3203d0bc1a
Guruprasad3n <mr.guruprasadb@gmail.com>
2023-03-14 12:27:43 +0530

    Auth Middleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,13 +1,29 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = async(req, res, next)=>{
+   try{
+
+    const {userId} = req.body
     const token = req.headers['authorization'].split(" ")[1]
     jwt.verify(token, process.env.JWT_SECRET, (err, decode)=>{
         if(err){
-            return res.status(200).send({message:`auth Failed`, success:false})
+            return res.status(200).send({
+                message:'Auth Failed',
+                success:false
+            })
         }
         else{
-            // req.body.userId : decode.id
+            req.body.userId  = decode.id
+            next()
         }
     })
+
+
+   }catch(e){
+    console.log(e)
+    return res.status(401).send({
+        message:'Auth Failed',
+        success:false
+    })
+   }
 }
